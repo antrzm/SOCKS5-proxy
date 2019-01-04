d60cd5dcc823c913c6b0af72a54b0c5aa519ff8c
Антон Разумов <a.razumov@g.nsu.ru>
2019-01-04 17:47:50 +0700

    fixed proxy(now it's working); magic numbers are static final now; some other changes in code

---
+++ src/ClientChannelHandler.java
@@ -0,0 +1,224 @@
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.SocketChannel;
+
+import org.xbill.DNS.*;
+
+
+class ClientChannelHandler {
+    private final static int BUFFER_SIZE = 8 * 1024;
+
+    private final static int SOCKS5 = 0x05;
+    private final static int AUTH_NUM = 0x01;               //greeting message
+    private final static int NO_AUTH = 0x00;                //greeting message
+    private final static int TCP_IP_STREAM_CONNECTION = 0x01;
+    private final static int RESERVED = 0x00;
+
+    private final static int IPV4 = 0x01;
+    private final static int DNS = 0x03;
+
+    private final static int SIZE_GREETINGS = 2;
+    private final static int SIZE_CONNECTION = 10;
+    private final static int SIZE_IP = 4;
+    private final static int REQ_GRANTED = 0x00;
+    private final static int ERROR = 0x01;
+
+    private SocketChannel localSocketChannel;
+    private SocketChannel remoteSocketChannel;
+
+    private boolean destroy = false;
+
+    private static final int STAGE_DEFAULT = 0;
+    private static final int STAGE_REGISTERED = 1;
+    private static final int STAGE_CONNECTED = 2;
+
+    private int stage;
+
+    private InetAddress address;
+    private int remotePort;
+
+    ClientChannelHandler(SocketChannel socketChannel) throws IOException {
+        this.localSocketChannel = socketChannel;
+        socketChannel.configureBlocking(false);
+        stage = STAGE_DEFAULT;
+    }
+
+    void remoteRead() throws IOException {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(BUFFER_SIZE);
+        try {
+            if (remoteSocketChannel.isConnected()) {
+                int numBytes = remoteSocketChannel.read(byteBuffer);
+                if (numBytes > 0) {
+                    localSocketChannel.write(ByteBuffer.wrap(byteBuffer.array(), 0, numBytes));
+                } else if (numBytes == -1) {
+                    this.destroy = true;
+                }
+            }
+        } catch (IOException e) {
+            if (localSocketChannel.isConnected())
+                localSocketChannel.close();
+            if (remoteSocketChannel.isConnected())
+                remoteSocketChannel.close();
+        } finally {
+            byteBuffer.clear();
+        }
+    }
+
+    void localRead() throws IOException {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(BUFFER_SIZE);
+        try {
+            int bytes = localSocketChannel.read(byteBuffer);
+            byteBuffer.flip();
+            if (stage == STAGE_DEFAULT) {
+                if (bytes > 0) {
+                    int version = byteBuffer.get();
+                    if (version != SOCKS5) {
+                        System.out.println("Incorrect version: " + version);
+                        throw new IOException();
+                    }
+                    int authNum = byteBuffer.get();
+                    int authMethod = byteBuffer.get();
+                    if (authNum != AUTH_NUM || authMethod != NO_AUTH) {
+                        throw new IOException();
+                    }
+
+                    ByteBuffer buf = ByteBuffer.allocate(SIZE_GREETINGS);
+                    buf.put((byte) SOCKS5);
+                    buf.put((byte) NO_AUTH);
+                    localSocketChannel.write(ByteBuffer.wrap(buf.array()));
+                    buf.clear();
+                    stage = STAGE_REGISTERED;
+                } else if (bytes == -1) {
+                    throw new IOException();
+                }
+            } else if (stage == STAGE_REGISTERED) {
+                if (bytes > 0) {
+                    int version = byteBuffer.get();
+                    if (version != SOCKS5) {
+                        System.out.println("Incorrect version: " + version);
+                        throw new IOException();
+                    }
+
+                    int commandCode = byteBuffer.get();
+                    int reserved = byteBuffer.get();
+                    if (commandCode != TCP_IP_STREAM_CONNECTION || reserved != RESERVED) {
+                        throw new IOException();
+                    }
+
+                    int addressType = byteBuffer.get();
+                    if (addressType == IPV4) {
+                        byte[] ip = new byte[SIZE_IP];
+                        byteBuffer.get(ip);
+                        address = InetAddress.getByAddress(ip);
+                    } else if (addressType == DNS) {
+                        int len = byteBuffer.get();
+                        byte[] byteName = new byte[len];
+                        byteBuffer.get(byteName);
+                        String nameStr = new String(byteName);
+
+                        Name name = Name.fromString(nameStr, Name.root);
+                        Record record = Record.newRecord(name, Type.A, DClass.IN);
+                        Message message = Message.newQuery(record);
+                        Main.dnsChannel.write(ByteBuffer.wrap(message.toWire()));
+                        Main.dns.put(message.getHeader().getID(), this);
+                    } else {
+                        throw new IOException();
+                    }
+
+                    remotePort = byteBuffer.getShort();
+
+                    if (addressType == IPV4) {
+                        connect();
+                    }
+
+                } else if (bytes == -1) {
+                    throw new IOException();
+                }
+            } else if (stage == STAGE_CONNECTED) {
+                if (localSocketChannel.isConnected()) {
+                    if (bytes > 0) {
+                        remoteSocketChannel.write(ByteBuffer.wrap(byteBuffer.array(), 0, bytes));
+                    } else if (bytes == -1) {
+                        throw new IOException();
+                    }
+                }
+            }
+        } catch (IOException e) {
+            this.destroy = true;
+            if (localSocketChannel.isConnected())
+                localSocketChannel.close();
+            if (remoteSocketChannel != null && remoteSocketChannel.isConnected())
+                remoteSocketChannel.close();
+        } finally {
+            byteBuffer.clear();
+        }
+    }
+
+    void connect(InetAddress address) throws IOException {
+        this.address = address;
+        this.connect();
+    }
+
+    private void connect() throws IOException {
+        try {
+            if (localSocketChannel.isConnected()) {
+                remoteSocketChannel = SocketChannel.open(new InetSocketAddress(this.address, remotePort));
+                ByteBuffer buffer = ByteBuffer.allocate(SIZE_CONNECTION);
+
+                buffer.put((byte) SOCKS5);
+                buffer.put(remoteSocketChannel.isConnected() ? (byte) REQ_GRANTED : (byte) ERROR);
+                buffer.put((byte) RESERVED);
+                buffer.put((byte) IPV4);
+                buffer.put(InetAddress.getLocalHost().getAddress());
+                buffer.putShort((short) Main.port);
+
+                localSocketChannel.write(ByteBuffer.wrap(buffer.array()));
+                buffer.clear();
+
+                if (!remoteSocketChannel.isConnected()) {
+                    this.destroy = true;
+                    return;
+                }
+                remoteSocketChannel.configureBlocking(false);
+                remoteSocketChannel.register(Main.selector, SelectionKey.OP_READ | SelectionKey.OP_CONNECT);
+                Main.remotes.put(remoteSocketChannel, this);
+                stage = STAGE_CONNECTED;
+            }
+        } catch (ConnectException e) {
+            System.out.println(e.getMessage());
+            if (localSocketChannel.isConnected())
+                localSocketChannel.close();
+            if (remoteSocketChannel != null && remoteSocketChannel.isConnected())
+                remoteSocketChannel.close();
+        } finally {
+            if (!this.localSocketChannel.isConnected()) this.destroy = true;
+        }
+    }
+
+    boolean isDestroy() {
+        return destroy;
+    }
+
+    void destroy() {
+        if (this.localSocketChannel != null) {
+            try {
+                this.localSocketChannel.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (this.remoteSocketChannel != null) {
+            try {
+                this.remoteSocketChannel.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 import java.io.IOException;
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
 import java.util.HashMap;
@@ -11,49 +9,55 @@ import java.util.Map;
 
 import org.xbill.DNS.*;
 
-import handler.*;
-
 public class Main {
 
     private final static int BUFFER_SIZE = 8 * 1024;
-    private static int port;
-    private static InetAddress host;
+    static int port;
 
-    private static Selector selector;
+    static Selector selector;
     private static ServerSocketChannel serverSocketChannel = null;
 
-    private static SocketChannel dnsSocket;
+    static DatagramChannel dnsChannel;
 
-    private static Map<SocketChannel, SocketChannel> clients = new HashMap<>();
-    private static Map<SocketChannel, SocketChannel> servers = new HashMap<>();
-
+    private static Map<SocketChannel, ClientChannelHandler> clients = new HashMap<>();
+    static Map<SocketChannel, ClientChannelHandler> remotes = new HashMap<>();
+    static HashMap<Integer, ClientChannelHandler> dns = new HashMap<>();
 
     public static void main(String args[]) {
-        if (args.length < 2) {
-            System.err.println("Not enough args\nShould be <host> <port>");
-            System.exit(-1);
+        if (args.length < 1) {
+            System.err.println("No args were set. Port is 1080");
+            port = 1080;
+        } else {
+            try {
+                port = Integer.valueOf(args[0]);
+                if (port < 0 || port > 65535) {
+                    throw new NumberFormatException();
+                }
+            } catch (NumberFormatException nfe) {
+                System.err.println("Port value is not correct. Using default (1080)");
+                port = 1080;
+            }
         }
         try {
-            port = Integer.valueOf(args[1]);
-            host = InetAddress.getByName(args[0]);
-        } catch (NumberFormatException e) {
-            System.err.println("Incorrect args");
-        } catch (UnknownHostException e) {
-            System.err.println("Unknown host " + args[2]);
-        }
-        try {
+            //selector & serverSocketChannel settings:
             selector = Selector.open();
             serverSocketChannel = ServerSocketChannel.open();
             serverSocketChannel.configureBlocking(false);
-            serverSocketChannel.bind(new InetSocketAddress(host, port));
+            serverSocketChannel.bind(new InetSocketAddress(port));
             serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+            //dns settings:
             String dnsServers[] = ResolverConfig.getCurrentConfig().servers();
-            dnsSocket = SocketChannel.open();
-            dnsSocket.configureBlocking(false);
-            if (dnsServers == null) dnsSocket.connect(new InetSocketAddress("8.8.8.8", 53));
-            else dnsSocket.connect(new InetSocketAddress(dnsServers[0], 53));
-            dnsSocket.register(selector, SelectionKey.OP_READ | SelectionKey.OP_CONNECT);
-            while (!dnsSocket.isConnected()) dnsSocket.finishConnect();
+            dnsChannel = DatagramChannel.open();
+            dnsChannel.configureBlocking(false);
+
+            if (dnsServers.length > 2) {
+                dnsChannel.connect(new InetSocketAddress(dnsServers[2], 53));
+            } else {
+                dnsChannel.connect(new InetSocketAddress("8.8.8.8", 53));
+            }
+            dnsChannel.register(selector, SelectionKey.OP_READ);
+
             startServer();
         } catch (IOException e) {
             e.printStackTrace();
@@ -61,193 +65,80 @@ public class Main {
         }
     }
 
-    private static final byte[] OK = new byte[]{0x05, 0x00, 0x00, 0x03, /*dns name*/0x00, /*port*/0x00, 0x00, 0x00};
-    private static final byte[] GREETING_OK = new byte[]{0x05, (byte) 0xFF};
-
-    private static byte[] CURRENT_RESP = null;
-
     private static void startServer() throws IOException {
-        List<ClientChannelHandler> handlerList = new LinkedList<ClientChannelHandler>();
         System.out.println("Server started");
-        while (true) {
-            selector.select();
-            for (SelectionKey key : selector.selectedKeys()) {
-                if (key.isValid()) {
-                    try {
-                        if (key.isConnectable()) System.out.println("hui");
-                        if (key.isAcceptable()) {
-                            ServerSocketChannel serverSocket = (ServerSocketChannel) key.channel();
-                            SocketChannel socketChannel = serverSocket.accept();
-                            if (socketChannel != null)
-                                handlerList.add(new ClientChannelHandler(selector, socketChannel, dnsSocket));
-                            //   accept(key);
-                        } else {
+        try {
+            while (true) {
+                selector.select();
+                for (SelectionKey key : selector.selectedKeys()) {
+                    if (key.isValid()) {
+                        if (key.isAcceptable() && serverSocketChannel == key.channel()) {
+                            SocketChannel socketChannel = serverSocketChannel.accept();
+                            if (socketChannel != null) {
+                                ClientChannelHandler handler = new ClientChannelHandler(socketChannel);
+                                clients.put(socketChannel, handler);
+                                socketChannel.register(selector, SelectionKey.OP_READ);
+                            }
+                        } else if (key.isConnectable()) ((SocketChannel) key.channel()).finishConnect();
+                        else if (key.isReadable()) {
                             List<ClientChannelHandler> toDestroyList = new LinkedList<>();
-                            for (ClientChannelHandler handler : handlerList) {
-                                handler.handle(key);
-                                if (handler.isDestroy()) {
-                                    toDestroyList.add(handler);
+                            if (key.channel() instanceof SocketChannel) { //not a DatagramChannel
+                                SocketChannel socketChannel = (SocketChannel) key.channel();
+                                ClientChannelHandler handler = clients.get(socketChannel);
+                                if (handler != null) {
+                                    handler.localRead();
+                                } else {
+                                    handler = remotes.get(socketChannel);
+                                    if (handler != null) {
+                                        handler.remoteRead();
+                                    }
+                                }
+                                if (handler != null)
+                                    if (handler.isDestroy()) {
+                                        toDestroyList.add(handler);
+                                    }
+                            } else if (key.channel().equals(dnsChannel)) {  //not a SocketChannel
+                                ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
+                                int length = dnsChannel.read(buffer);
+                                if (length > 0) {
+                                    Message message = new Message(buffer.array());
+                                    Record[] records = message.getSectionArray(1);
+                                    for (Record record : records) {
+                                        if (record instanceof ARecord) {
+                                            ARecord aRecord = (ARecord) record;
+                                            int id = message.getHeader().getID();
+                                            ClientChannelHandler handler = dns.get(id);
+                                            if (handler != null && aRecord.getAddress() != null) {
+                                                handler.connect(aRecord.getAddress());
+                                                if (handler.isDestroy()) {
+                                                    toDestroyList.add(handler);
+                                                }
+                                            }
+                                            dns.remove(id);
+                                            break;
+                                        }
+                                    }
+                                    buffer.clear();
                                 }
                             }
                             for (ClientChannelHandler handler : toDestroyList) {
                                 handler.destroy();
-                                handlerList.remove(handler);
                             }
                         }
-//                        } else if (key.isConnectable()) {
-//                            connect(key);
-//                        } else if (key.isWritable()) {
-//                            write(key);
-//                        } else if (key.isReadable()) {
-//                            read(key);
-//                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        close(key);
+
                     }
                 }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            for (SocketChannel channel : clients.keySet()) {
+                channel.close();
+            }
+            serverSocketChannel.close();
+            dnsChannel.close();
+            selector.close();
         }
     }
+
 }
-/*
-    static SocketChannel dstChannel;
-
-    private static void accept(SelectionKey key) throws IOException {
-        SocketChannel newChannel = ((ServerSocketChannel) key.channel()).accept();
-        if (newChannel != null) {
-            newChannel.configureBlocking(false);
-            newChannel.register(key.selector(), SelectionKey.OP_READ);
-
-            SocketChannel tmpChannel = SocketChannel.open();
-            tmpChannel.configureBlocking(false);
-            tmpChannel.connect(new InetSocketAddress(port));
-            tmpChannel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_CONNECT);
-
-            dstChannel = tmpChannel;
-        }
-    }
-
-    private static void read(SelectionKey key) throws IOException {
-        SocketChannel channel = ((SocketChannel) key.channel());
-        Attachment attachment = ((Attachment) key.attachment());
-        if (attachment == null) {
-            key.attach(attachment = new Attachment());
-            attachment.in = ByteBuffer.allocate(BUFFER_SIZE);
-        }
-        if (channel.read(attachment.in) < 1) {
-            close(key);
-        } else if (attachment.peer == null) {
-            readHeader(key, attachment);
-        } else {
-            //proxy
-            attachment.peer.interestOps(attachment.peer.interestOps() | SelectionKey.OP_WRITE);
-            // а у первого убираем интерес прочитать, т.к пока не записали текущие данные, читать ничего не будем
-            key.interestOps(key.interestOps() ^ SelectionKey.OP_READ);
-            // готовим буфер для записи
-            attachment.in.flip();
-        }
-    }
-
-    private static void readHeader(SelectionKey key, Attachment attachment) throws IllegalStateException, IOException {
-        byte[] ar = attachment.in.array();
-        if (ar[attachment.in.position() - 1] == 0) {
-            // Если последний байт \0 это конец ID пользователя.
-            if (ar[0] != 5 || ar[1] != 1 || (ar[3] != 3 && ar[3] != 0 && ar[3] != 1)) {
-                throw new IllegalStateException("Bad Request");
-            }
-
-            //TODO: add case: ar[3] == 1
-
-            int dnsNameLen = ar[4];
-
-            StringBuilder sb = new StringBuilder();
-            String host = "";
-
-
-            for (int i = 0; i < dnsNameLen; i++) {
-                int ch = ar[5 + i];
-                sb.append((char) ch);
-            }
-            host = sb.toString();
-
-            int c = ar[5 + dnsNameLen];
-            int p = (c << 8);
-            c = ar[6 + dnsNameLen];
-            p += c;
-
-            switch (ar[3]) {
-                case 0:             //Greeting
-                    byte greeting[] = new byte[]{0x05, 0x00};
-                    dstChannel.write(ByteBuffer.wrap(greeting));
-                    break;
-                case 1:             //IPv4
-                    byte ok[] = ar;
-                    ok[1] = 0x00;
-                    dstChannel.write(ByteBuffer.wrap(ok));
-                    break;
-                case 3:             //DNS
-                    Name addr = Name.fromString(host);
-
-                    break;
-                default:
-                    throw new IllegalStateException("Bad Request");
-            }
-        }
-    }
-
-    private static void write(SelectionKey key) throws IOException {
-        // Закрывать сокет надо только записав все данные
-        SocketChannel channel = ((SocketChannel) key.channel());
-        Attachment attachment = ((Attachment) key.attachment());
-        if (channel.write(attachment.out) == -1) {
-            close(key);
-        } else if (attachment.out.remaining() == 0) {
-            if (attachment.peer == null) {
-                // Дописали что было в буфере и закрываемся
-                close(key);
-            } else {
-                // если всё записано, чистим буфер
-                attachment.out.clear();
-                // Добавялем ко второму концу интерес на чтение
-                attachment.peer.interestOps(attachment.peer.interestOps() | SelectionKey.OP_READ);
-                // А у своего убираем интерес на запись
-                key.interestOps(key.interestOps() ^ SelectionKey.OP_WRITE);
-            }
-        }
-    }
-
-    private static void connect(SelectionKey key) throws IOException {
-        SocketChannel channel = ((SocketChannel) key.channel());
-        Attachment attachment = ((Attachment) key.attachment());
-        // Завершаем соединение
-        channel.finishConnect();
-        // Создаём буфер и отвечаем OK
-        attachment.in = ByteBuffer.allocate(BUFFER_SIZE);
-        attachment.in.put(CURRENT_RESP).flip();
-        if (attachment.peer != null) {
-            attachment.out = ((Attachment) attachment.peer.attachment()).in;
-            ((Attachment) attachment.peer.attachment()).out = attachment.in;
-            // Ставим второму концу флаги на на запись и на чтение
-            // как только она запишет OK, переключит второй конец на чтение
-            attachment.peer.interestOps(SelectionKey.OP_WRITE | SelectionKey.OP_READ);
-        }
-        key.interestOps(0);
-    }
-
-    private static void close(SelectionKey key) throws IOException {
-        key.cancel();
-        key.channel().close();
-        SelectionKey peerKey = null;
-        if (key.attachment() != null)
-            peerKey = ((Attachment) key.attachment()).peer;
-        if (peerKey != null) {
-            ((Attachment) peerKey.attachment()).peer = null;
-            if ((peerKey.interestOps() & SelectionKey.OP_WRITE) == 0) {
-                if (((Attachment) peerKey.attachment()).out != null)
-                    ((Attachment) peerKey.attachment()).out.flip();
-            }
-            peerKey.interestOps(SelectionKey.OP_WRITE);
-        }
-    }
-}*/
